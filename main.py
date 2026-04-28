f89f78114e24d543ab0b8e2964438dff679f62db
trishar8130 <trishar8130@gmail.com>
2026-04-28 08:26:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ age=st.number_input("Enter your age:")
 
 #Display a message when button is clicked
 if st.button("Submit"):
-  st.write("Hello,(name)!Welcome to streamlit")
+  st.write("Hello,(name),!Welcome to streamlit")
